5699cf0dfa6429c5e9f1fbabf54a17d735630b5f
hugoresende27 <Hugoresende27@gmail.com>
2021-11-30 13:51:27 +0000

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,4 +1,14 @@
 -- TRIGGERS
+-- mysql -u root na linha de comandos
 CREATE TABLE trigger_test (
 	message VARCHAR (100)
 	);
+	
+delimiter $$ -- mudo  o delimiter para $$
+CREATE 
+	TRIGGER my_trigger BEFORE INSERT 
+	ON employee
+	FOR EACH ROW BEGIN 
+		INSERT INTO trigger_test VALUES ("Adiciona novo empregado");
+	END$$ -- termina a instruçao
+delimiter ; -- volto a por delimiter como ;
