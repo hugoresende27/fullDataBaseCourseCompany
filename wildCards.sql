050da947e08781999e77308626349aeb2f2b1759
hugoresende27 <Hugoresende27@gmail.com>
2021-12-26 11:43:04 +0000

    Update wildCards.sql

--- wildCards.sql
+++ wildCards.sql
@@ -7,4 +7,4 @@ SELECT * FROM branch_supplier WHERE supplier_name LIKE '% label%';
 -- encontra employee nascido em outubro
 SELECT * FROM employee WHERE birth_day LIKE  "____-02%";
 -- encontrar clientes q são escolas
-SELECT * FROM client WHERE client_name LIKE "%school%";
+SELECT * FROM client WHERE client_name LIKE "%school%";hrsistemausers
